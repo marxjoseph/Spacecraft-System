ac27751aa0c197039030b3dfc4753d9a0c69dc5b
Joseph Marques <marquesjoseph03@gmail.com>
2026-09-07 14:12:37 -0400

    Push

--- distance.py
+++ distance.py
@@ -1,13 +1,8 @@
 from time import sleep
 from gpiozero import DistanceSensor
 
-sensor = DistanceSensor(echo=13, trigger=12, max_distance=4)
+sensor = DistanceSensor(echo=13, trigger=12)
 
-print("Reading distance from sensor...")
-
-try:
-    while True:
-        print(f"Distance: {sensor.distance * 100:.1f} cm")
-        sleep(1)
-except KeyboardInterrupt:
-    print("Stopped by user.")
+while True:
+    print(sensor.distance * 100, "cm")
+    sleep(1)
