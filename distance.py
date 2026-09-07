d218be008e6b1f915b4b06a8a606926e4ad3b850
marxjoseph <162480216+marxjoseph@users.noreply.github.com>
2026-09-07 14:37:34 -0400

    Implement distance measurement using GPIO
    Replaced DistanceSensor with GPIO setup for distance measurement.

--- distance.py
+++ distance.py
@@ -1,8 +1,32 @@
-from time import sleep
-from gpiozero import DistanceSensor
+import RPi.GPIO as GPIO
+import time
 
-sensor = DistanceSensor(echo=13, trigger=12)
+GPIO.setmode(GPIO.BCM)
 
-while True:
-    print(sensor.distance * 100, "cm")
-    sleep(1)
+GPIO_TRIG = 11
+GPIO_ECHO = 18
+
+GPIO.setup(GPIO_TRIG, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+GPIO.output(GPIO_TRIG, GPIO.LOW)
+
+time.sleep(2)
+
+GPIO.output(GPIO_TRIG, GPIO.HIGH)
+time.sleep(0.00001)
+GPIO.output(GPIO_TRIG, GPIO.LOW)
+
+while GPIO.input(GPIO_ECHO) == 0:
+    start_time = time.time()
+
+while GPIO.input(GPIO_ECHO) == 1:
+    bounce_back_time = time.time()
+
+pulse_duration = bounce_back_time - start_time
+
+distance = round(pulse_duration * 17150, 2)
+
+print("Distance:", distance, "cm")
+
+GPIO.cleanup()
